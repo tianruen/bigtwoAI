2fc3f726883d52470ad9db04e3f6461f829bd64a
WTTien <wtiantien@hotmail.com>
2024-06-19 20:11:17 +0100

    Cards now clickable?

--- ui.py
+++ ui.py
@@ -5,16 +5,40 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.behaviors import ButtonBehavior
 
-class CardWidget(BoxLayout):
+
+class imgbtn(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(imgbtn, self).__init__(**kwargs)
+
+    def on_press(self):
+        print("Button pressed", self.source)
+        CardDeckApp.l1.text = self.source
+
+class lblbtn(ButtonBehavior, Label):
+    def __init__(self, **kwargs):
+        super(lblbtn, self).__init__(**kwargs)
+
+    def on_press(self):
+        print("Button pressed", self.text)
+        CardDeckApp.l1.text = self.text
+
+
+class CardWidget(ButtonBehavior, BoxLayout):
     def __init__(self, rank, suit_image):
         super(CardWidget, self).__init__(orientation='vertical',padding=10,spacing=10)
 
-        self.rank_label = Label(text=rank, font_size=50, size_hint=(None, None), size=(50, 50))
-        self.suit_image = Image(source=suit_image, size_hint=(None, None), size=(50, 50))
+        self.rank_label = lblbtn(text=rank, font_size=50, size_hint=(None, None), size=(50, 50))
+        self.suit_image = imgbtn(source=suit_image, size_hint=(None, None), size=(50, 50))
         
         self.add_widget(self.rank_label)
         self.add_widget(self.suit_image)
+
+    def on_press(self):
+        print("Pressed")
+        CardDeckApp.l1 = self.rank_label
+
 
 class Deck:
     def __init__(self):
@@ -41,13 +65,21 @@ class CardDeckApp(App):
         for card in cards:
             layout.add_widget(card)
 
-        scroll_view = ScrollView(size_hint=(None, None), size=(800, 600))
+        scroll_view = ScrollView(size_hint=(None, None), size=(1000, 800))
         scroll_view.add_widget(layout)
 
         root = BoxLayout(orientation='horizontal')
         root.add_widget(scroll_view)
-        return root
 
+        CardDeckApp.l1 = Label(text='Test', font_size=32)
+       
+        root.add_widget(CardDeckApp.l1)
+        
+        
+        disp = GridLayout(cols = 1)
+        disp.add_widget(root)
+
+        return disp
 
 if __name__ == "__main__":
     CardDeckApp().run()
