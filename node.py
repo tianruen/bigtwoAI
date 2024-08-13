3b1c817292450d58847769d0e7593f7cfe580e29
WTTien <wtiantien@hotmail.com>
2024-08-14 01:30:35 +0800

    Change the way to use itertools.combination, with lesser code. Finding FourOfAKind and ThreeOfAKind method for 5 cards combination is created.

--- node.py
+++ node.py
@@ -2,154 +2,138 @@ from collections import defaultdict
 import itertools
 from big_two_AI_draft import Card, Hand
 
-class Node:
+# class Node:
 
-    '''
-    The Node class represents a node of the MCTS tree.
-    It contains the information needed for the algorithm to run its search.
-    '''
+#     '''
+#     The Node class represents a node of the MCTS tree.
+#     It contains the information needed for the algorithm to run its search.
+#     '''
 
-    def __init__(self, game, observation, done, parent, action_index):
+#     def __init__(self, game, observation, done, parent, action_index):
 
-        # Next move / next state (Child node)
-        self.child = None
+#         # Next move / next state (Child node)
+#         self.child = None
 
-        # Win count
-        self.T = 0
+#         # Win count
+#         self.T = 0
 
-        # Visit count
-        self.N = 0
+#         # Visit count
+#         self.N = 0
 
-        # Current game (Including actions allowed, render results etc.)
-        self.game = game
+#         # Current game (Including actions allowed, render results etc.)
+#         self.game = game
 
-        # Observation of current game state, describing cards played?
-        self.observation = observation
+#         # Observation of current game state, describing cards played?
+#         self.observation = observation
 
-        # If game is won/loss/draw
-        self.done = done
+#         # If game is won/loss/draw
+#         self.done = done
 
-        # link to parent node
-        self.parent = parent
+#         # link to parent node
+#         self.parent = parent
 
-        # index of action leading to this node 
-        # (Actions parent nodes took)        
-        self.action_index = action_index
+#         # index of action leading to this node 
+#         # (Actions parent nodes took)        
+#         self.action_index = action_index
 
-def born(self):
+# def born(self):
 
-    if self.done:
-        return
+#     if self.done:
+#         return
     
-    actions = []
-    games = []
+#     actions = []
+#     games = []
     
-    # Cycle through possible actions and create a list
-    # List also include the current game state
-    for i in range(current_Game_State.available_actions):
-        actions.append(i)
-        new_game = self.current_Game_State
-        games.append(new_game)
+#     # Cycle through possible actions and create a list
+#     # List also include the current game state
+#     for i in range(current_Game_State.available_actions):
+#         actions.append(i)
+#         new_game = self.current_Game_State
+#         games.append(new_game)
 
     
-    child = {}
-    for action, game in range zip(actions,games):
-        # Play the card (action) and advance the game (game)
-        game.play(action) # Game should be in different state after this
+#     child = {}
+#     for action, game in range zip(actions,games):
+#         # Play the card (action) and advance the game (game)
+#         game.play(action) # Game should be in different state after this
 
-        observation = game.cards
-        done = game.end
+#         observation = game.cards
+#         done = game.end
         
-        # Then create a child node
-        child = Node(game, observation, done, self, action)
+#         # Then create a child node
+#         child = Node(game, observation, done, self, action)
 
 
-def choose_action():
+# def choose_action():
     
-    available_moves = []
+#     available_moves = []
     
-    cur_cards = game.cur_card
-    cards_length = len(cur_cards)
+#     cur_cards = game.cur_card
+#     cards_length = len(cur_cards)
     
-    player_hand = game.cur_player.hand
+#     player_hand = game.cur_player.hand
     
-    if cards_length == 1:
-        for tobeplay_card in player_hand:
-            hand = Hand(tobeplay_card)
-            if hand.compare(cur_cards):
-                available_moves.append(tobeplay_card)
+#     if cards_length == 1:
+#         for tobeplay_card in player_hand:
+#             hand = Hand(tobeplay_card)
+#             if hand.compare(cur_cards):
+#                 available_moves.append(tobeplay_card)
     
-    elif cards_length == 2 or cards_length == 3:
-        combinations = filter_cards_combinations_23(player_hand, cards_length)
-        for tobeplay_cards in combinations:
-            hand = Hand(tobeplay_cards)
-            if hand.compare(cur_cards):
-                available_moves.append(tobeplay_cards)
+#     elif cards_length == 2 or cards_length == 3:
+#         combinations = filter_cards_combinations_23(player_hand, cards_length)
+#         for tobeplay_cards in combinations:
+#             hand = Hand(tobeplay_cards)
+#             if hand.compare(cur_cards):
+#                 available_moves.append(tobeplay_cards)
     
-    elif cards_length == 5:
-        ## HOW DAFUQ can we figure out all 5 cards combo..
-        # 4 of a kind:
-            # Filter out rank with 4 cards
-            # Combine it with remaining cards
+#     elif cards_length == 5:
+#         ## HOW DAFUQ can we figure out all 5 cards combo..
+#         # 4 of a kind:
+#             # Filter out rank with 4 cards
+#             # Combine it with remaining cards
         
-        # 3 of a kind:
-            # Filter out rank with 3 cards (We call this 'group rank A')
-            # Filter out rank with 2 cards (Including other rank with 3 or 4 cards except 'group rank A')
-            # Find all combination for this rank group if the rank has 3 or 4 cards
-            # Combine them
+#         # 3 of a kind:
+#             # Filter out rank with 3 cards (We call this 'group rank A')
+#             # Filter out rank with 2 cards (Including other rank with 3 or 4 cards except 'group rank A')
+#             # Find all combination for this rank group if the rank has 3 or 4 cards
+#             # Combine them
 
-            # Filter out rank with 4 cards (We call this 'group rank B')
-            # Find all 3 cards combination for 'group rank B'
-            # Filter out rank with 2 cards (Including other rank with 3 or 4 cards except 'group rank B')
-            # Find all combination for this rank group if the rank has 3 or 4 cards
-            # Combine them
+#             # Filter out rank with 4 cards (We call this 'group rank B')
+#             # Find all 3 cards combination for 'group rank B'
+#             # Filter out rank with 2 cards (Including other rank with 3 or 4 cards except 'group rank B')
+#             # Find all combination for this rank group if the rank has 3 or 4 cards
+#             # Combine them
         
-        # tonghua/tonghua shun:
-            # Combine cards based on suits
-            # Find all combination for suits with more than or equal 5 cards
+#         # tonghua/tonghua shun:
+#             # Combine cards based on suits
+#             # Find all combination for suits with more than or equal 5 cards
         
-        # straight:
+#         # straight:
         
-def filter_cards_combinations_23(player_hand, card_length):
+# def filter_cards_combinations_23(player_hand, card_length):
     
-    grouped_cards = defaultdict(list)
+#     grouped_cards_rank = defaultdict(list)
     
-    ## Group all cards with same rank 
-    for card in player_hand:
-        r = Card.rank2val[card.rank]
-        grouped_cards[r].append(card)
+#     ## Group all cards with same rank 
+#     for card in player_hand:
+#         r = Card.rank2val[card.rank]
+#         grouped_cards_rank[r].append(card)
     
-    ## Filter away group with quantity lower than current card_length
-    ## They can't be played
-    temp_filtered_groups = [group for group in grouped_cards.values() if len(group) >= card_length]
-    filtered_groups = []
+#     ## Filter away group with quantity lower than current card_length
+#     ## They can't be played
+#     temp_filtered_groups = [group for group in grouped_cards_rank.values() if len(group) >= card_length]
+#     filtered_groups = []
 
-    for group in temp_filtered_groups:
+#     # Find all possible combinations following card_length
+#     # Using itertools.combinations
+#     for group in temp_filtered_groups:
         
-        ## Just include the group if they have the same quantity as current card_length
-        
-        ### TODO: Test if this line is needed or cover in next condition , changing > to >=?
-        if len(group) == card_length:
-            filtered_groups.append(group)
-        
-        
-        # If a rank has more cards than card_length, 
-        # consider all case
-        # Example: card_length is 2, but we have 3D, 3C, 3H
-        # We include all combinations into filtered_groups
-        
-        elif len(group) > card_length:
+#         filtered_groups.extend(itertools.combinations(group, card_length))
 
-            ### TODO: Test if we can straight change combinations to filtered_group?
-            combinations = list(itertools.combinations(group, card_length))
-            
-            ### Uncomment line below if want to change tuple item to list
-            # combinations = [list(item) for item in combinations]
-            
-            for combo in combinations:
-                filtered_groups.append(combo)
+#         # change filtered_groups from iterables to list
+#         filtered_groups = [list(item) for item in filtered_groups]
     
-    return filtered_groups
+#     return filtered_groups
 
 def filter_cards_combinations_5(player_hand):
     
@@ -161,59 +145,117 @@ def filter_cards_combinations_5(player_hand):
         r = Card.rank2val[card.rank]
         grouped_cards_rank[r].append(card)
     
+    add_FourOfAKindCombo(grouped_cards_rank, five_card_combo)
+    add_ThreeOfAKindCombo(grouped_cards_rank, five_card_combo)
 
-    # 4 of a kind
-    dish = [group for group in grouped_cards_rank.values() if len(group) == 4]
+    # ranks = Card.ranks
+
+    # meal = []
+
+    # for i in range(len(ranks) - 4):
+    #     possible_straight_ranks = ranks[i:i+5]
+
+    #     possible_meal = []
+    #     for rank in possible_straight_ranks:
+    #         if grouped_cards_rank[rank]:
+    #             possible_meal.append(grouped_cards_rank[rank])
+    #         else:
+    #             break
     
-    for food in dish:
-        sauce = grouped_cards_rank
-        sauce.remove(food)
+    #     if len(possible_meal) == 5:
+    #         meal.extend(itertools.product(*possible_meal))
+    #         meal = [list(item) for item in meal]
+    #         five_card_combo.append(meal)
 
+    
+    return five_card_combo
+
+def add_FourOfAKindCombo(grouped_cards_rank, five_card_combo):
+    # 4 of a kind , LETS GO
+    
+    # grouped_cards_rank is a dictionary with ranks as key and the value are all the cards with the rank
+    # find groups that has at least four cards
+    # Name these groups DISH
+    dish = [group for group in grouped_cards_rank.values() if len(group) == 4]
+
+    for food in dish:
+        sauce = grouped_cards_rank.copy()
+
+        # Exclude group of the chosen rank
+        # Then sauce.remove(food)
+        # But dict doesn't allow it to be easy, so we do following
+        # Shit
+        for key, value in sauce.items():
+            if value == food:
+                del sauce[key]
+                break
+
+        # Now we transform sauce from dict to list
+        # Without key(rank)
+        # SAUCE is now single cards to be paired with DISH
+        sauce = [item for value_list in sauce.values() for item in value_list]
+
+        # PAIR THE SHIT OUT OF THEM, AND MAKE A FKING MEAL
         for liao in sauce:
-            meal = food
-            meal.extend(liao)
+            meal = food.copy()
+            meal.append(liao)                    # Can't do extend because liao is single Card object, not iterable list of Card object(?)
             five_card_combo.append(meal)
 
-    # 3 of a kind
+def add_ThreeOfAKindCombo(grouped_cards_rank, five_card_combo):
+    # 3 of a kind , LETS GO
+
+    # grouped_cards_rank is a dictionary with ranks as key and the value are all the cards with the rank
+    # find groups that has at least three cards
+    # Name these groups TEMP_DISH, because we are going to do some combo
     temp_dish = [group for group in grouped_cards_rank.values() if len(group) >= 3]
     dish = []
 
+    # TEMP_DISH goes through combo
+    # and we have fucking dishes
+    # group DISH contains all possible 3 card combinations
     for plate in temp_dish:
-        if len(plate) == 3:
-            dish.append(plate)
-        elif len(plate) > 3:
-            combinations = list(itertools.combinations(plate, 3))
-            for combo in combinations:
-                dish.append(combo)
+        dish.extend(itertools.combinations(plate, 3))
+        dish = [list(item) for item in dish]
     
     for food in dish:
-        temp_sauce = [group for group in grouped_cards_rank.values() if len(group) >= 2]
-        temp_sauce.remove(food[r])
+        temp_sauce = grouped_cards_rank.copy()
+
+        # Exclude group of the chosen rank
+        # Concept is like temp_sauce.remove(food)
+        for key, value in temp_sauce.items():
+            if all(item in value for item in food):
+                del temp_sauce[key]
+                break
+        # TEMP_SAUCE group now contains all the group except chosen rank group
+
+        # Find groups that has at least two cards
+        # At the same time we transform TEMP_SAUCE from dict to list
+        temp_sauce = [group for group in temp_sauce.values() if len(group) >= 2]
+        
         sauce = []
 
+        # TEMP_SAUCE goes through combo
+        # and we have fucking sauces
+        # group SAUCE contains all possible 2 card combinations
         for bowl in temp_sauce:
-            if len(bowl) == 2:
-                sauce.append(bowl)
-            elif len(bowl) > 2:
-                combinations = list(itertools.combinations(bowl, 2))
-                for combo in combinations:
-                    sauce.append(combo)
+            sauce.extend(itertools.combinations(bowl, 2))
+            sauce = [list(item) for item in sauce]
         
+        # PAIR THE SHIT OUT OF THEM, AND MAKE A FKING MEAL
         for liao in sauce:
-            meal = food
-            meal.extend(liao)
+            meal = food.copy()
+            meal.extend(liao)                   # Can't do append because it creates list within list
             five_card_combo.append(meal)
-            
 
 if __name__ == "__main__":
-    p1 = input("First player name: ")
-    p2 = input("Second player name: ")
-    p3 = input("Third player name: ")
-    p4 = input("Fourth player name: ")
-    players = [p1,p2,p3,p4]
+    # p1 = input("First player name: ")
+    # p2 = input("Second player name: ")
+    # p3 = input("Third player name: ")
+    # p4 = input("Fourth player name: ")
+    # players = [p1,p2,p3,p4]
 
-    game = BigTwoGame(players)
-    game.play_game()        
+    # game = BigTwoGame(players)
+    # game.play_game()        
 
 # Do a play, with one player as AI, choosing card randomly
 
@@ -221,19 +263,25 @@ if __name__ == "__main__":
 
     #### TESTING FOR FILTER_CARDS_COMBINATIONS : 2 and 3 cards####
     
-    # player_hand = []
-    # player_hand.append(Card('Diamonds','3'))
-    # player_hand.append(Card('Clubs','3'))
-    # player_hand.append(Card('Hearts','3'))
-    # player_hand.append(Card('Diamonds','4'))
-    # player_hand.append(Card('Spades','5'))
-    # player_hand.append(Card('Diamonds','6'))
-    # player_hand.append(Card('Clubs','6'))
-    # player_hand.append(Card('Hearts','6'))
-    # player_hand.append(Card('Spades','6'))
+    player_hand = []
+    player_hand.append(Card('Diamonds','3'))
+    player_hand.append(Card('Clubs','3'))
+    player_hand.append(Card('Hearts','3'))
+    player_hand.append(Card('Spades','3'))
+    player_hand.append(Card('Diamonds','4'))
+    player_hand.append(Card('Spades','5'))
+    player_hand.append(Card('Diamonds','6'))
+    player_hand.append(Card('Clubs','6'))
+    player_hand.append(Card('Hearts','7'))
+    player_hand.append(Card('Spades','8'))
+    player_hand.append(Card('Diamonds','J'))
+    player_hand.append(Card('Clubs','J'))
+    player_hand.append(Card('Hearts','J'))
+    player_hand.append(Card('Spades','J'))
 
-    # result = filter_cards_combinations(player_hand, 3)
+    result = filter_cards_combinations_5(player_hand)
 
-    # print(result)
+    for stuff in result:
+        print (stuff)
 
     #### TESTING FOR FILTER_CARDS_COMBINATIONS : 2 and 3 cards ####
