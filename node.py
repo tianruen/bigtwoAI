67e5a9b45a3945f3043176bf08d32b63aefdea65
WTTien <wtiantien@hotmail.com>
2024-08-17 01:04:38 +0800

    Moved available_actions function to a new file for clarity.

--- node.py
+++ node.py
@@ -1,5 +1,5 @@
 from big_two_AI_draft import Card, Hand
-from findingCombo import Combo
+from availableActions import Actions
 
 # class Node:
 
@@ -61,40 +61,6 @@ from findingCombo import Combo
         
 #         # Then create a child node
 #         child = Node(game, observation, done, self, action)
-
-
-def available_actions(game):
-    
-    available_moves = []
-    
-    cur_cards = game.cur_card
-    cards_length = len(cur_cards)
-
-    player_hand = game.cur_player.hand
-    
-    if cards_length == 1:
-        for tobeplay_card in player_hand:
-            hand = []
-            hand.append(tobeplay_card)
-            hand = Hand(hand)                   # Would have error of Card not is not an iterable if we do "hand = Hand(tobeplay_card)" directly
-            if hand.compare(cur_cards):
-                available_moves.append(tobeplay_card)
-    
-    elif cards_length == 2 or cards_length == 3:
-        combinations = Combo.filter_cards_combinations_23(player_hand, cards_length)
-        for tobeplay_cards in combinations:
-            hand = Hand(tobeplay_cards)
-            if hand.compare(cur_cards):
-                available_moves.append(tobeplay_cards)
-    
-    elif cards_length == 5:
-        combinations = Combo.filter_cards_combinations_5(player_hand)
-        for tobeplay_combo in combinations:
-            hand = Hand(tobeplay_combo)
-            if hand.compare(cur_cards):
-                available_moves.append(tobeplay_combo)
-
-    return available_moves
 
 #### TESTING FOR AVAILABLE_ACTIONS####
 class game:
@@ -191,7 +157,7 @@ if __name__ == "__main__":
     cur_card.append(Card('Spades','5'))
     A.cur_card = cur_card
 
-    result = available_actions(A)
+    result = Actions.available_actions(A)
 
     for stuff in result:
         print(stuff)
