6ad2c1bb51337b3b4c37f850fc50c82618df1083
WTTien <wtiantien@hotmail.com>
2024-08-27 00:41:29 +0800

    Transform all single cards to list in available actions. Required to be processed by game. None(Skip action) appended at last)

--- availableActions.py
+++ availableActions.py
@@ -4,26 +4,23 @@ from findingCombo import Combo
 class Actions:
 
     def available_actions(game):
-        
-        if game.first_move == True:
-            available_actions = []
-        else:
-            available_actions = [None]          # Avoid playing skip in the first move of the game
-        
+           
+        available_actions = []
+
         cur_cards = game.cur_card
 
         if game.players.index(game.cur_player) == 0:
             playing_hand = game.cur_player.hand
         else:
-            playing_hand = game.players[1].hand
+            playing_hand = game.players[1].hand.copy()
             playing_hand.extend(game.players[2].hand)
             playing_hand.extend(game.players[3].hand)       # We focus on devising moves for player[0]
 
         if not cur_cards:
-            available_actions.extend([card for card in playing_hand])                        # Add possible Single plays
-            available_actions.extend(Combo.filter_cards_combinations_23(playing_hand, 2))    # Add possible Double plays
-            available_actions.extend(Combo.filter_cards_combinations_23(playing_hand, 3))    # Add possible Triple plays
-            available_actions.extend(Combo.filter_cards_combinations_5(playing_hand))        # Add possible Five Cards combo
+            available_actions.extend([[card] for card in playing_hand])                         # Add possible Single plays
+            available_actions.extend(Combo.filter_cards_combinations_23(playing_hand, 2))       # Add possible Double plays
+            available_actions.extend(Combo.filter_cards_combinations_23(playing_hand, 3))       # Add possible Triple plays
+            available_actions.extend(Combo.filter_cards_combinations_5(playing_hand))           # Add possible Five Cards combo
         else:
             cards_length = len(cur_cards)
 
@@ -35,6 +32,9 @@ class Actions:
                 available_actions = findTriple(playing_hand, cur_cards)
             elif cards_length == 5:
                 available_actions = findFiveCardsCombo(playing_hand, cur_cards)
+        
+        if not game.first_move:
+            available_actions.append(None)        # Avoid playing skip in the first move of the game
         
         return available_actions
     
@@ -48,7 +48,7 @@ def findSingle(playing_hand, cur_cards):
         hand.append(tobeplay_card)
         hand = Hand(hand)                   # Would have error of Card not is not an iterable if we do "hand = Hand(tobeplay_card)" directly
         if hand.compare(cur_cards):
-            available_moves.append(tobeplay_card)
+            available_moves.append([tobeplay_card])
     
     return available_moves
 
