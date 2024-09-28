f6e33fdc12f155509e0b6bdc770ddb3296d41b6b
tianruen <tianruen.woon@gmail.com>
2024-09-28 23:35:03 +0100

    update file, to take note: how to define reward?

--- mcts1.py
+++ mcts1.py
@@ -24,7 +24,7 @@ class ModifyGame(bt.BigTwoGame):
         self.players.append(self.bot)
                 
     def next_player(self, cur_player):
-        if self.bot_play > 3:
+        if self.bot_play == 3:
             self.bot_play = 0
             return self.players[0] # since the agent is always the first player
         else:
@@ -33,63 +33,37 @@ class ModifyGame(bt.BigTwoGame):
         
 class BigTwoState():
     def __init__(self, game:ModifyGame) -> None:
-        self.game = copy.deepcopy(game)
+        self.game = game
         self.player = self.game.cur_player
         self.table_cards = next((p_c[1] for p_c in self.game.game_hist[-3:][::-1] if p_c[1] is not None), None)   # p_c: (player, played_cards)
         
-    def available_actions_indi(self, hand_cards:List[List[bt.Card]]):
-        first_round = []
-        if self.table_cards is not None:
-            cards_length = len(self.table_cards)
-            if cards_length == 1:
-                available_actions = bt.findSingle(hand_cards, self.table_cards)
-            elif cards_length == 2:
-                available_actions = bt.findDouble(hand_cards, self.table_cards)
-            elif cards_length == 3:
-                available_actions = bt.findTriple(hand_cards, self.table_cards)
-            elif cards_length == 5:
-                available_actions = bt.findFiveCardsCombo(hand_cards, self.table_cards)
-            available_actions.append(None)
-        else:
-            available_actions = []
-            available_actions.extend([[card] for card in hand_cards])                         # Add possible Single plays
-            available_actions.extend(bt.Combo.filter_cards_combinations_23(hand_cards, 2))       # Add possible Double plays
-            available_actions.extend(bt.Combo.filter_cards_combinations_23(hand_cards, 3))       # Add possible Triple plays
-            available_actions.extend(bt.Combo.filter_cards_combinations_5(hand_cards))           # Add possible Five Cards combo
-            first_round = [act for act in available_actions if bt.Card("Diamonds", "3") in act]
-        return first_round if first_round else available_actions
-    
     def get_available_actions(self):
-        if self.player.type in ["Agent", "AI"]:
-            return self.available_actions_indi(self.player.hand)
-        else:
-            available_actions = []
-            for player in self.game.players:
-                if player.type not in ["Agent", "AI"]:
-                    available_actions.extend(self.available_actions_indi(player.hand))
-            return available_actions
-    
+        return self.player.get_available_actions(self.table_cards)
+        
     def move(self, action:List[bt.Card]):
         self.game.play_turn(action)
         return BigTwoState(self.game)
     
     def is_terminal(self):
         return self.game.game_over()
-        # return len(self.game.cur_player.hand) == 0
         
     def get_reward(self):
         winner_idx = [len(p.hand) == 0 for p in self.game.players].index(True)
-        return 1 if self.game.players[winner_idx].type in ["Agent", "AI"] else 0
+        # return 1 if self.game.players[winner_idx].type in ["Agent", "AI"] else 0
         # return 1 if the agent (with index 0) win the game, otherwise 0
+        
+        return len(self.game.bot.hand) - len(self.game.players[0].hand)     # game.players[0] is the agent
 
 class MCTSNode():
-    def __init__(self, state:BigTwoState, parent:Optional["MCTSNode"]=None, parent_action:List[bt.Card]=None) -> None:
+    def __init__(self, state:BigTwoState, parent:Optional["MCTSNode"]=None) -> None:
         self.state = state      # cards that are played
         self.parent = parent
-        self.parent_action = parent_action
         self.children = []
         self.num_visits = 0
         self.num_wins = 0
+    
+    def __repr__(self) -> str:
+        return f"{self.state.game.game_hist[-1][1]}" if len(self.state.game.game_hist) > 0 else "None"
         
     def is_fully_expanded(self):
         return len(self.children) == len(self.state.get_available_actions())
@@ -104,9 +78,11 @@ class MCTSNode():
             
     def expand(self):
         action = random.choice(self.state.get_available_actions())
-        next_state = self.state.move(action)
-        child_node = MCTSNode(state=next_state, parent=self, parent_action=action)
-        self.children.append(child_node)
+        state_ = copy.deepcopy(self.state)  # to avoid changing the state of the current node
+        next_state = state_.move(action)
+        child_node = MCTSNode(state=next_state, parent=self)
+        if all([repr(child_node) != repr(child) for child in self.children]):
+            self.children.append(child_node)
         return child_node
     
     def backpropagate(self, reward):
@@ -119,7 +95,7 @@ class MCTS():
     def __init__(self, root:MCTSNode) -> None:
         self.root = root
     
-    def search(self, num_iterations=10):
+    def search(self, num_iterations=10) -> MCTSNode:
         result = 0
         for i in range(num_iterations):
             node = self.tree_policy()
@@ -127,9 +103,9 @@ class MCTS():
             node.backpropagate(reward)
             
             result += reward
-            print(result, "/", i+1)
+            # print(result, "/", i+1)
             
-        return self.root.best_child()
+        return self.root.best_child(c_param=0)
     
     def tree_policy(self) -> MCTSNode:
         node = self.root
@@ -144,6 +120,7 @@ class MCTS():
         state_ = copy.deepcopy(state)
         while not state_.is_terminal():
             action = random.choice(state_.get_available_actions())
+            # print("Player: ", state_.player.name, "Action: ", action, "Table cards: ", state_.table_cards)
             state_ = state_.move(action)
         return state_.get_reward()
 
@@ -152,75 +129,10 @@ if __name__ == "__main__":
     t1, t2, t3, t4 = "Agent", "Bot", "Bot", "Bot"
     p_t = zip([p1,p2,p3,p4], [t1,t2,t3,t4])
     game = ModifyGame(p_t)
-    print(game.cur_player)
+    # print(game.cur_player)
     
     state = BigTwoState(game)
     root = MCTSNode(state)
     mcts_ = MCTS(root)
     best_action = mcts_.search()
-    
-    
-    
-    
-    
-    
-# class Node:
-#     def __init__(self, state, parent=None):
-#         self.state = state
-#         self.parent = parent
-#         self.children = []
-#         self.visits = 0
-#         self.value = 0
-
-#     def is_fully_expanded(self):
-#         return len(self.children) == len(self.state.get_legal_actions())
-
-#     def best_child(self, c_param=1.4):
-#         choices_weights = [
-#             (child.value / child.visits) + c_param * math.sqrt((2 * math.log(self.visits) / child.visits))
-#             for child in self.children
-#         ]
-#         return self.children[choices_weights.index(max(choices_weights))]
-
-#     def expand(self):
-#         action = random.choice(self.state.get_legal_actions())
-#         next_state = self.state.move(action)
-#         child_node = Node(next_state, self)
-#         self.children.append(child_node)
-#         return child_node
-
-#     def update(self, reward):
-#         self.visits += 1
-#         self.value += reward
-
-# class MCTS:
-#     def __init__(self, root):
-#         self.root = root
-
-#     def search(self, iterations=1000):
-#         for _ in range(iterations):
-#             node = self.tree_policy()
-#             reward = self.default_policy(node.state)
-#             self.backup(node, reward)
-#         return self.root.best_child(c_param=0)
-
-#     def tree_policy(self):
-#         node = self.root
-#         while not node.state.is_terminal():
-#             if not node.is_fully_expanded():
-#                 return node.expand()
-#             else:
-#                 node = node.best_child()
-#         return node
-
-#     def default_policy(self, state):
-#         while not state.is_terminal():
-#             action = random.choice(state.get_legal_actions())
-#             state = state.move(action)
-#         return state.get_reward()
-
-#     def backup(self, node, reward):
-#         while node is not None:
-#             node.update(reward)
-#             node = node.parent
-            
+    print(best_action)            
