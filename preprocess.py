787fa677db07743029fc999143930fed59ddb0bd
tianruen <tianruen.woon@gmail.com>
2024-10-10 18:42:50 +0100

    add func to calculate opponents' number of cards

--- preprocess.py
+++ preprocess.py
@@ -9,9 +9,16 @@ class Preprocess:
         self.game_hist = game.game_hist
         self.all_cards = [bt.Card(suit, rank) for rank in bt.Card.ranks for suit in bt.Card.suits]
         
-    def get_hand_card(self) -> List:
+    def get_hand_cards(self) -> List:
         hand_card = set(self.cur_player.hand)
         return [1 if card in hand_card else 0 for card in self.all_cards]
+    
+    def get_opponent_num_cards(self):
+        opponents = []
+        for player in self.game.players:
+            if player.name != self.cur_player.name:
+                opponents.extend([1 if len(player.hand) == i else 0 for i in range(1,14)])
+        return opponents
 
 
 
