cf157b8f049a0b62c508bcffa007787fba17a986
tianruen <tianruen.woon@gmail.com>
2024-10-25 15:13:19 +0100

    sort available_actions in Player.get_available_actions()

--- big_two_AI.py
+++ big_two_AI.py
@@ -71,6 +71,7 @@ class Player:
                 available_actions = findTriple(self.hand, table_cards)
             elif cards_length == 5:
                 available_actions = findFiveCardsCombo(self.hand, table_cards)
+            available_actions = sorted([sorted(act) for act in available_actions])
             available_actions.append(None)
         else:
             available_actions = []
@@ -78,6 +79,7 @@ class Player:
             available_actions.extend(Combo.filter_cards_combinations_23(self.hand, 2))       # Add possible Double plays
             available_actions.extend(Combo.filter_cards_combinations_23(self.hand, 3))       # Add possible Triple plays
             available_actions.extend(Combo.filter_cards_combinations_5(self.hand))           # Add possible Five Cards combo
+            available_actions = sorted([sorted(act) for act in available_actions])
             first_round = [act for act in available_actions if Card("Diamonds", "3") in act]
         return first_round if first_round else available_actions
     # available_actions if [Card("Diamonds", "3")] not in available_actions else first_round
